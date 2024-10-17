7f1d107d04b266b61394c325e47f95f5580abbb1
intermediatecoder <bhadaneadi.23@gmail.com>
2024-10-17 09:22:30 +0530

    comments the question

--- Question1/ShippingCompany.js
+++ Question1/ShippingCompany.js
@@ -1,3 +1,26 @@
+/*Question 1
+Use Case: Shipping Company Optimization
+Background
+A shipping company needs to efficiently allocate various shipments into 
+available containers. Each shipment consists of a specific number of units, 
+and each container has a maximum capacity of units it can hold. The goal 
+is to determine the minimum number of containers required to 
+accommodate all shipments, even if units from the same shipment are split 
+across multiple containers.
+Scenario
+Company: SwiftLogistics
+Shipments: The company has n shipments to be delivered. Each shipment 
+consists of a different number of units. This is represented by the array 
+shipments where shipments[i] indicates the number of units in the ith shipment.
+Containers: The company also has m containers available for use, each 
+with a specific capacity. This is represented by the array 
+container_limits where container_limits[j] indicates the 
+maximum number of units that the j-th container can hold.
+Problem
+The operations team at SwiftLogistics needs to figure out the minimum 
+number of containers required to redistribute the units from all shipments 
+into the available containers*/ 
+
 function Container_needed(a, c) {
     
     let Shipments = 0;
--- Question2/Linked_list.js
+++ Question2/Linked_list.js
@@ -1,4 +1,18 @@
-
+/*Question 2
+Question-Scenario:
+You are a software developer at a logistics company. Your company 
+maintains a system to track packages as they move through different 
+stages of delivery. Each package's journey is represented as a linked list, 
+where each node contains information about a stage in the delivery 
+process.
+Recently, some packages have been reported to be stuck in an infinite loop 
+in the tracking system, causing delays and confusion. To prevent this, you 
+need to implement a function to detect any structural issues in the package 
+tracking system.
+Problem Statement:
+Given the head of a linked list representing the stages of a package's 
+journey, you need to determine if there is a structural issue causing an 
+infinite loop. */
 
 class Node {
     constructor(x) {
